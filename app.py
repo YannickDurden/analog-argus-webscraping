ddf57cb9496adabf8e3143514a141f8cd50abe73
Yannick <y.francois54@gmail.com>
2022-03-07 10:20:01 +0100

    add cors headers

--- app.py
+++ app.py
@@ -27,3 +27,18 @@ def search_and_compute_price():
         price['status'] = 404
 
     return jsonify(price)
+
+
+@app.after_request
+def add_cors_headers(response):
+    r = request.referrer[:-1]
+    white_list = ['http://localhost:4200', 'https://analog-argus.netlify.app']
+    if r in white_list:
+        response.headers.add('Access-Control-Allow-Origin', r)
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+        response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
+        response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT')
+    return response
